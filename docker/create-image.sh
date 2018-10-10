47860c4760e3ecb67d8b1f7700a53ca846a45886
Alex Moros Marco <alex.moros.marco@gmail.com>
2018-10-10 21:42:44 +0200

    build context moved to a variable to better understand what the code does

--- docker/create-image.sh
+++ docker/create-image.sh
@@ -1,5 +1,7 @@
 # Builds the image using this folder Dockerfile
-# Names it with the given image name/repository (a tag can be added in the format <image name>:<tag>)
+# Names it with the given image name/repository (a tag can be added in the
+# format <image name>:<tag>)
 # Sets the build context to the above folder
 image_name="spa-template"
-docker build -f Dockerfile -t $image_name ../
+build_context="../"
+docker build -f Dockerfile -t $image_name $build_context
